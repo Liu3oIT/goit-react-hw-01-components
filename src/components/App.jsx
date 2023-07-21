d9ca60b792e6ba6a7510959064d85f94474dcff6
Liu3oIT <liutyivova995@gmail.com>
2023-07-22 01:04:42 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
+import { Profile, Statistics, FriendList, TransactionHistory } from './profile';
+import data from '../data.json';
+import user from '../user.json';
+import friends from '../friends.json';
+import transactions from '../transactions.json';
 export const App = () => {
   return (
     <div
       style={{
         height: '100vh',
-        display: 'flex',
+        // display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+      <Statistics data={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory transactions={transactions} />
     </div>
   );
 };
